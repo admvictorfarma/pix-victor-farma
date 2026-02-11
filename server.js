779546a0eb2bcaca8632a5a09f534b4ebcf80d35
Admin Victor Farma <victor@victorfarma.com>
2026-02-11 16:11:13 -0300

    Fix: corrigir importação CommonJS do mercadopago para ES Module

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import crypto from 'crypto';
-import { MercadoPagoConfig, Payment } from 'mercadopago';
+import mercadopago from 'mercadopago';
+
+const { MercadoPagoConfig, Payment } = mercadopago;
 
 dotenv.config();
 
